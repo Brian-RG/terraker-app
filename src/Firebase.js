99bac8a456ba1008235b4141b8bd40179e2dbc69
Brian Reyes Gálvez <brianreyesg@gmail.com>
2019-10-17 20:53:03 -0500

    fix error
    Signed-off-by: Brian Reyes Gálvez <brianreyesg@gmail.com>

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,17 +2,5 @@ import * as firebase from 'firebase';
 //import firestore from 'firebase/firestore';
 
 
-
-const config = {
-    apiKey: "AIzaSyCqvzN6RUPRgeaoMPAznODuYQU13ul4ojg",
-    authDomain: "terraker-blogs.firebaseapp.com",
-    databaseURL: "https://terraker-blogs.firebaseio.com",
-    projectId: "terraker-blogs",
-    storageBucket: "terraker-blogs.appspot.com",
-    messagingSenderId: "795755721463",
-    appId: "1:795755721463:web:818b69b1942b7a288d59f2",
-    measurementId: "G-N71ND72200"
-};
-
 firebase.initializeApp(config);
 export default firebase;
